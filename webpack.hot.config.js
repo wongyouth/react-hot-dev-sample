f0c3a37f0240af51b559437f5e1a67ca2fad005f
Ryan Wang <wongyouth@gmail.com>
2015-09-09 23:53:55 +0800

    separate hot config from normal config

--- webpack.hot.config.js
+++ webpack.hot.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: './app/index',
   output: {
     path: path.join(__dirname, "build"),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    publicPath: "/build/"
   },
   resolve: {
     extensions: ["", ".js", ".jsx"]
@@ -13,7 +14,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ["babel-loader"],
+        loaders: ["react-hot", "babel-loader"],
         // loaders: ["babel-loader"],
         exclude: /node_modules/
       }
